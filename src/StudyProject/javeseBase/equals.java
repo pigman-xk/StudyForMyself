753d8fa91f0429cff1ebbe86ead188e0207fe662
pigman-xk <201830720162@mail.scut.edu.cn>
2021-05-08 16:54:33 +0800

    equals的要点和空指针equals更优的处理办法

--- src/StudyProject/javeseBase/equals.java
+++ src/StudyProject/javeseBase/equals.java
@@ -1,11 +1,14 @@
 package StudyProject.javeseBase;
+
+import java.util.Objects;
+
 /*
 * 任何类型的空指针：类型 变量名 = null，调用equals()会报错误，应使用常量或确定有值的对象来调用equals()方法，
 * 而new的集合也不会报错，集合的equals()比较的是对象之间的地址是否相等
 * 创建好对象的引用类型则不会，因为默认调用了Object.equals(),该方法中已经默认处理的该种空指针，故不会报错。
 *且集合和Object对象的equals是同一个.
 *
-* 处理办法：使用Object.equals(a,b) 该方法已经做好空指针的处理，不会报错
+* 处理办法：使用Objects.equals(a,b) 该方法已经做好空指针的处理，不会报错(注意是Objects，而不是Object)
 * */
 public class equals {
     public static void main(String[] args) {
@@ -21,6 +24,7 @@ public class equals {
             list_s.equals("abc");//报错
             o.equals("abc");//报错
             s.equals("abc");//报错
+        System.out.print(Objects.equals(s,o));//不报错,返回True
 
 //        }catch(Exception e){
 //            e.printStackTrace();
